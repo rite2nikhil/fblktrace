81874b6f24459a659f4efcc97b2c413232a5f755
Nikhil Bhatia <rite2nikhil@gmail.com>
2019-08-06 17:53:38 -0700

    report

--- report.py
+++ report.py
@@ -10,19 +10,34 @@ def getFileName(inum, dict):
         dict[inum]=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read().rstrip()
     return dict[inum]
         
+def split_list(n):
+    """will return the list index"""
+    return [(x+1) for x,y in zip(n, n[1:]) if y-x != 1]
+
+def get_sub_list(my_list):
+    """will split the list base on the index"""
+    my_index = split_list(my_list)
+    output = list()
+    prev = 0
+    for index in my_index:
+        new_list = [ x for x in my_list[prev:] if x < index]
+        output.append(new_list)
+        prev += len(new_list)
+    output.append([ x for x in my_list[prev:]])
+    return output
+
 
 def main():
    filepath = sys.argv[1]
    d = dict()
    b = dict()
-   print(filepath)
    if not os.path.isfile(filepath):
        print("File path {} does not exist. Exiting...".format(filepath))
        sys.exit()
   
    with open(filepath) as fp:
        for line in fp:
-           print(line)
+           #print(line)
            args=line.strip().split('\t')
            if args.__len__() < 3:
                continue
@@ -30,17 +45,18 @@ def main():
            fileName=getFileName(inum, d)
            if fileName == '':
                continue
-           print(inum, d[inum])
+           #print(inum, d[inum])
            fsblk_parts=args[2].split("=")
            if fsblk_parts.__len__() != 2:
                continue
            fsblk = fsblk_parts[1]
            b.setdefault(inum, []).append(fsblk)
 
-   for key, val in d.items():
-       if val != '':
-           print(key)
-           print(val, b[key])
+   for inum in d:
+       file=d[inum]
+       if file == '':
+           continue
+        print(os.path.relpath(file, "/var/lib/kubelet/pods/"), get_sub_list(b[inum]))
     
 if __name__ == '__main__':
    main()
